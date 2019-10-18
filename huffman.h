1957a8f432dcfcdc7061f3f77ce78789f701928d
sunchaohui <tulipforyou@mail.hfut.edu.cn>
2019-10-18 14:03:47 +0800

    save

---
+++ huffman.h
@@ -0,0 +1,16 @@
+/*
+Designed by sch
+time:2019.10.18
+本文件用于哈夫曼编码测试用例
+哈夫曼编码：常用于数据解压缩的大数据量传输工作中
+*/
+
+#ifndef _HUFFMAN_H
+#define _HUFFMAN_H
+
+#include <iostream>
+using namespace std;
+
+//暂无
+
+#endif
